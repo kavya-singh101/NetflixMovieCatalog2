3d01d407433e402cfbe5944a96191316af8d0488
Kavya Singh <kavya_singh@kavya.singh>
2024-09-18 21:45:02 +0530

    cicd

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ with open('data/data_movies.json', 'r') as f:
 
 @app.route("/", methods=['GET'])
 def home():
-    return "Helloooo! This app is an API, there is no UI ;-)"
+    return "Hello! This app is an API, there is no UI ;-)"
 
 
 @app.route('/discover')
