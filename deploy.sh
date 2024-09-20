5d78b279c0d98f6cd2f8a215912f2b0203d17a30
Kavya Singh <kavya_singh@kavya.singh>
2024-09-20 11:54:10 +0530

    hello

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 # Step 1: Delete all Docker containers forcefully
 echo "Stopping and removing all Docker containers..."
-docker rm -f $(docker ps -aq)
+sudo docker rm -f $(docker ps -aq)
 
 # Step 2: Delete all Docker images
 echo "Removing all Docker images..."
-docker rmi -f $(docker images -aq)
+sudo docker rmi -f $(docker images -aq)
 
 # Step 3: Remove existing repository and its child directories
 REPO_PATH="home/ubuntu/NetflixMovieCatalog2"  # Replace with the actual path to your repo
@@ -15,14 +15,14 @@ REPO_PATH="home/ubuntu/NetflixMovieCatalog2"  # Replace with the actual path to 
 # Step 5: Enter the newly cloned repository
 cd "$REPO_PATH" || exit
 git pull https://github.com/kavya-singh101/NetflixMovieCatalog2.git
-echo "Entered the repository directory: $(pwd)"
+sudo echo "Entered the repository directory: $(pwd)"
 
 # Step 6: Build the Docker image using the Dockerfile
 echo "Building Docker image from the Dockerfile..."
-docker build -t netflix_movie_catalog .
+sudo docker build -t netflix_movie_catalog .
 
 # Step 7: Run a container from the built image
 echo "Running the Docker container..."
-docker run -d -p 80:8080 --name movie_catalog netflix_movie_catalog
+sudo docker run -d -p 80:8080 --name movie_catalog netflix_movie_catalog
 
 echo "Process completed successfully!"
